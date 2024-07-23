0112fc81888cf511d64170b6182a269129e51138
nghes <voasd00@gmail.com>
2024-07-23 11:38:38 +0700

    adjust dto/bookentry folder

--- Application/Dtos/BookEntry/BookEntryDto.cs
+++ Application/Dtos/BookEntry/BookEntryDto.cs
@@ -1,25 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace BookManagementSystem.Application.Dtos.BookEntry
 {
     //properties of the BookEntryDto: EntryID, BookID, quantity
-    public class CreateBookEntryDto
-    {
-        public int quantity { get; set; }
-    }
+    
 
-    public class UpdateBookEntryDto
-    {
-        public int quantity { get; set; }
-    }
+    
     public class BookEntryDto
     {
         public string? EntryID { get; set;}
-        public string? BookID { get; set; }
-        public int quantity { get; set; }
+        public string? EntryDate { get; set; }
+        
     }
 
 
---
+++ Application/Dtos/BookEntry/UpdateBookEntryDto.cs
@@ -0,0 +1,9 @@
+namespace BookManagementSystem.Application.Dtos.BookEntry
+{
+    //properties of the BookEntryDto: EntryID, BookID, quantity
+    
+    public class UpdateBookEntryDto
+    {
+        public string? EntryDate { get; set; }
+    }
+}
