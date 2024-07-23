567163a8a83ba196358708c581c30734a6b460bb
nghes <voasd00@gmail.com>
2024-07-23 11:41:27 +0700

    clean comment in dto/bookentry files

--- Application/Dtos/BookEntry/BookEntryDto.cs
+++ Application/Dtos/BookEntry/BookEntryDto.cs
@@ -1,16 +1,8 @@
 namespace BookManagementSystem.Application.Dtos.BookEntry
-{
-    //properties of the BookEntryDto: EntryID, BookID, quantity
-    
-
-    
+{ 
     public class BookEntryDto
     {
         public string? EntryID { get; set;}
         public string? EntryDate { get; set; }
-        
     }
-
-
-
 }
