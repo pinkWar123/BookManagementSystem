36d7568669e5fef357e9f96cfbb158cddf1aa3e8
Huy Trieu <huygigidokhongbiet@gmail.com>
2024-07-27 14:43:53 +0700

    fix DebtReportDetailRepository GetByIdAsync

--- Infrastructure/Repositories/DebtReportDetail/DebtReportDetailRepository.cs
+++ Infrastructure/Repositories/DebtReportDetail/DebtReportDetailRepository.cs
@@ -15,7 +15,8 @@ namespace BookManagementSystem.Infrastructure.Repositories.DebtReportDetail
         public async Task<Domain.Entities.DebtReportDetail?> GetByIdAsync(int reportId, int customerId)
         {
             return await _context.Set<Domain.Entities.DebtReportDetail>()
-                .FirstOrDefaultAsync(d => d.ReportID == reportId && d.CustomerID == customerId);
+                // .FirstOrDefaultAsync(d => d.ReportID == reportId && d.CustomerID == customerId);
+                .FindAsync(reportId, customerId);
         }
 
         public async Task<Domain.Entities.DebtReportDetail?> UpdateAsync<TUpdateDto>(int reportId, int customerId, TUpdateDto entity) where TUpdateDto : class
