de4fac6621167aeacc55600f40edcdeb196c5ce8
pinkWar123 <131501454+pinkWar123@users.noreply.github.com>
2024-07-29 15:44:04 +0700

    Update JWT token lifetime

--- Configuration/Settings/JWT.cs
+++ Configuration/Settings/JWT.cs
@@ -10,6 +10,6 @@ namespace BookManagementSystem.Settings
         public string? SigningKey { get; set; }
         public string? Issuer { get; set; }
         public string? Audience { get; set; }
-        public int DurationInMinutes { get; set; }
+        public int DurationInMinute { get; set; }
     }
 }
