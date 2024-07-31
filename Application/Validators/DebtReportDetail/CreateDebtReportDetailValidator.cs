44cdbb54d85cace1278c344e4545dbb2523ecb42
Huy Trieu <huygigidokhongbiet@gmail.com>
2024-07-31 09:51:12 +0700

    add Exception for create 2 Id

--- Application/Validators/DebtReportDetail/CreateDebtReportDetailValidator.cs
+++ Application/Validators/DebtReportDetail/CreateDebtReportDetailValidator.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using BookManagementSystem.Application.Dtos.DebtReportDetail;
 using FluentValidation;
 
@@ -7,6 +8,14 @@ namespace BookManagementSystem.Application.Validators
     {
         public CreateDebtReportDetailValidator()
         {
+            RuleFor(x => x.ReportID)
+                .NotEmpty().WithMessage("ID của báo cáo không được để trống.")
+                .GreaterThan(0).WithMessage("ID của báo cáo phải là số dương.");
+
+            RuleFor(x => x.CustomerID)
+                .NotEmpty().WithMessage("ID của khách hàng không được để trống.")
+                .GreaterThan(0).WithMessage("ID của khách hàng phải là số dương.");
+
             RuleFor(x => x.InitialDebt)
                 .NotEmpty().WithMessage("Số nợ ban đầu không được để trống.")
                 .GreaterThanOrEqualTo(0).WithMessage("Số nợ ban đầu phải là số không âm.");
