f77d02a8046d7d6473985c32fdb0d0622425138e
nghes <voasd00@gmail.com>
2024-08-02 22:44:39 +0700

    Erase ID of BookEntry and Invoice in CreateDto

--- Application/Dtos/BookEntry/CreateBookEntryDto.cs
+++ Application/Dtos/BookEntry/CreateBookEntryDto.cs
@@ -2,7 +2,6 @@ namespace BookManagementSystem.Application.Dtos.BookEntry
 {
     public class CreateBookEntryDto
     {
-        public int EntryID { get; set; }
         public string? EntryDate { get; set; }
     }
     
