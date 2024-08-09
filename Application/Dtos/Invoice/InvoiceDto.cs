1d880c994cfd717d08f54b950b5b342460ed6f9d
nghes <voasd00@gmail.com>
2024-08-09 22:40:24 +0700

    add some Invoice Service contidion

--- Application/Dtos/Invoice/InvoiceDto.cs
+++ Application/Dtos/Invoice/InvoiceDto.cs
@@ -3,7 +3,7 @@ namespace BookManagementSystem.Application.Dtos.Invoice
     public class InvoiceDto
     {
         public int Id { get; set; }
-        public DateOnly? InvoiceDate { get; set; }
+        public required DateOnly InvoiceDate { get; set; }
         public int? CustomerID { get; set; }
         
     }
--- Application/Services/InvoiceService.cs
+++ Application/Services/InvoiceService.cs
@@ -1,21 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using BookManagementSystem.Application.Dtos.Invoice;
 using BookManagementSystem.Application.Interfaces;
 using BookManagementSystem.Domain.Entities;
 using BookManagementSystem.Infrastructure.Repositories.Invoice;
-using BookManagementSystem.Application.Validators;
-using FluentValidation;
-using FluentValidation.Results;
 using BookManagementSystem.Application.Exceptions;
 using BookManagementSystem.Application.Queries;
 using Microsoft.EntityFrameworkCore;
-using BookManagementSystem.Infrastructure.Repositories.Customer;
-using BookManagementSystem.Infrastructure.Repositories.InvoiceDetail;
-using System.Runtime.InteropServices;
+using BookManagementSystem.Application.Dtos.Customer;
+using BookManagementSystem.Application.Dtos.DebtReportDetail;
+using BookManagementSystem.Data;
+using BookManagementSystem.Application.Dtos.Book;
 
 namespace BookManagementSystem.Application.Services
 {
@@ -23,29 +17,79 @@ namespace BookManagementSystem.Application.Services
     public class InvoiceService : IInvoiceService
     {
         private readonly IInvoiceRepository _invoiceRepository;
-        private readonly ICustomerRepository _customerRepository;
+        private readonly ICustomerService _customerService;
+        private readonly IRegulationService _regulationService;
+        private readonly IBookService _bookService;
+        private readonly IDebtReportService _debtReportService;
+        private readonly IDebtReportDetailService _debtReportDetailService;
         private readonly IMapper _mapper;
         public InvoiceService(
             IInvoiceRepository invoiceRepository, 
-            ICustomerRepository customerRepository,
-            IInvoiceDetailRepository invoiceDetailRepository,
+            ICustomerService customerService,
+            IRegulationService regulationService,
+            IBookService bookService,
+            IDebtReportService debtReportService,
+            IDebtReportDetailService debtReportDetailService,
             IMapper mapper
   )
         {
-            _customerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
+            _customerService = customerService;
             _invoiceRepository = invoiceRepository ?? throw new ArgumentNullException(nameof(invoiceRepository));
+            _regulationService = regulationService;
+            _bookService = bookService;
+            _debtReportService = debtReportService;
+            _debtReportDetailService = debtReportDetailService;
             _mapper = mapper;
         }
 
         public async Task<InvoiceDto> CreateNewInvoice(CreateInvoiceDto createInvoiceDto)
         {
             if (createInvoiceDto == null)
-                throw new ArgumentNullException(nameof(createInvoiceDto));
+                throw new InvoiceException("CreateInvoiceDto is required");
+            var invoiceDetails = createInvoiceDto.InvoiceDetails;
+            if (invoiceDetails == null)
+                throw new InvoiceException("InvoiceDetails is required");
             var invoice = _mapper.Map<Invoice>(createInvoiceDto);
-            Console.WriteLine("InvoiceID: " + invoice.Id);
-            var customer = await _customerRepository.GetByIdAsync(invoice.CustomerID);
+            var customer = await _customerService.GetCustomerById(invoice.CustomerID);
             if (customer == null)
                 throw new CustomerException($"Customer not found with ID {invoice.CustomerID}");
+            // Check if Customer is blocked
+            var regulation = await _regulationService.GetRegulationById(1);
+
+            // Update TotalDebt of Customer
+            var totalDebt = 0;
+            foreach (var detail in invoiceDetails)
+            {
+                int bookID = detail.BookID;
+                var book = await _bookService.GetBookById(bookID);
+                totalDebt += detail.Quantity * book.Price;
+            }
+            var updateCustomerDto = new UpdateCustomerDto
+            {
+                TotalDebt = totalDebt + customer.TotalDebt
+            };
+            await _customerService.UpdateCustomer(invoice.CustomerID, updateCustomerDto);
+
+            /// Update FinalDebt of DebtReportDetail
+            var invoiceDto = _mapper.Map<InvoiceDto>(invoice);
+            int reportId = await _debtReportService.GetReportIdByMonthYear(invoiceDto.InvoiceDate.Month, invoiceDto.InvoiceDate.Year);
+
+            var debtReportDetail = await _debtReportDetailService.GetDebtReportDetailById(reportId,invoice.CustomerID);
+            if (debtReportDetail == null)
+            {
+                throw new DebtReportDetailNotFound(reportId, invoice.CustomerID);
+            }
+            var updateDebtReportDetailDto = new UpdateDebtReportDetailDto
+            {
+                FinalDebt = debtReportDetail.FinalDebt + totalDebt
+            };
+            await _debtReportDetailService.UpdateDebtReportDetail(reportId, invoice.CustomerID, updateDebtReportDetailDto);
+
+
+
+
+
+
             await _invoiceRepository.AddAsync(invoice);
             await _invoiceRepository.SaveChangesAsync();
             if (createInvoiceDto.InvoiceDetails == null)
