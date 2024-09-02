7ca5df152e13247a6d9967835ea638e2df40aa51
Huy Trieu <huygigidokhongbiet@gmail.com>
2024-09-02 19:54:30 +0700

    add for adjust inital debt when fake data

--- Application/Dtos/DebtReportDetail/UpdateDebtReportDetailDto.cs
+++ Application/Dtos/DebtReportDetail/UpdateDebtReportDetailDto.cs
@@ -2,7 +2,8 @@ namespace BookManagementSystem.Application.Dtos.DebtReportDetail
 {
     public class UpdateDebtReportDetailDto
     {
-        // public int? InitialDebt { get; set; }
+        public int? InitialDebt { get; set; }
         public int? FinalDebt { get; set; }
+        public int? AdditionalDebt { get; set; }
     }
 }
